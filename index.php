340b550f21c57c6a05670cf8ea9dc78a4c140854
PiezoGo <piezo5go@gmail.com>
2024-07-16 22:08:57 +0300

    commit2

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include "./includes/Navbar.php" ?>
+<?php include "./includes/Header.php" ?>
+<div class="spice">
+    <img src="" alt="A picture if Hot pepper">
+    <p>Welcome to Spice</p>
+    <span>Spice up all your ideas with us!</span>
+    <button type="Submit">Join Our Community</button>
+</div>
+<?php include "./includes/Footer.php" ?>
+
