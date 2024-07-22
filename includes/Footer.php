7c9215528a22a8023c64988eb1bf4c20c62b43e5
PiezoGo <piezo5go@gmail.com>
2024-07-22 13:15:10 +0300

    COMMIT3

---
+++ includes/Footer.php
@@ -0,0 +1,24 @@
+<div class="footer">    
+    <div class="contact">   
+        <h6>Contact Us</h6>
+        <span>Mail us through <b>spiceups@spice.ac.ke</b></span>
+        <span>Call us.+254711344699</span>
+        <span>Follow us in our social media platforms</span>   
+    </div>
+
+    <div class="shortcuts">
+        <h6>Account</h6>
+        <span>Log in</span>
+        <span>Sign up</span>
+        <span>Your Spices</span>
+        <span>Projects</span>
+    </div>
+
+    <div class="about">
+        <h6>Quick Links</h6>
+        <span>Terms and Condition</span>
+        <span>Privacy Policy</span>
+        <span>FAQ</span>
+        <span>Help centre</span>
+    </div>
+</div>
