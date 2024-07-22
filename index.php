2778d7e230d01b9b5be47f76d58dcb325706fae3
PiezoGo <piezo5go@gmail.com>
2024-07-22 22:14:54 +0300

    commit6

--- index.php
+++ index.php
@@ -8,6 +8,26 @@
         <span>Michael is a well known Front-End web developer. 
             He peculiar in his renown skills in BootStrap, Javascript and Designing outstanding Websites.</span>
        <br> <button type="Submit">Make a Booking.</button>
+       <div class="more">
+            <div class="skills">
+                <h6>Skills</h6>
+                <ul>
+                    <li>Javascript</li>
+                    <li>BootStrap</li>
+                    <li>React</li>
+                    <li>Cascading Stylesheet</li>
+                </ul>
+            </div>
+            <div class="Hobbies">
+                <h6>Hobbies</h6>
+                <ul>
+                    <li>Coding</li>
+                    <li>Working with Database Systems</li>
+                    <li>Playing Lawn-Tennis</li>
+                    <li>Swimming</li>
+                </ul>
+            </div>
+       </div>
     </div>
 </div>
 <?php include "./includes/Footer.php" ?>
